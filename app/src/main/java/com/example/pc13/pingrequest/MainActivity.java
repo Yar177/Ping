a5399a7cfc828fdd46862cd3cd5701b9fab93a4a
Yar <yarsher.dev@gmail.com>
2019-02-28 22:36:56 -0500

    Permission request

--- app/src/main/java/com/example/pc13/pingrequest/MainActivity.java
+++ app/src/main/java/com/example/pc13/pingrequest/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.pc13.pingrequest;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 12111;
     TextView currentConectivity;
     TextView pingStatus;
     Button pingICMP;
@@ -27,10 +34,40 @@ public class MainActivity extends AppCompatActivity {
         pingICMP = (Button) findViewById(R.id.ping_icmp);
 
 
+        checkPermission();
+
+
+
+    }
+
+    void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+        }else {
+            curenrNetworkStatus();
+        }
 
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:{
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    curenrNetworkStatus();
+                } else {
+                    Toast.makeText(this, "You need permission", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
 
     void  curenrNetworkStatus(){
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
