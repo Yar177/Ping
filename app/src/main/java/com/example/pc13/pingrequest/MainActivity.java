eaa2c29fdf442ff613f32da2d64239074d1f3b98
Yar <yarsher.dev@gmail.com>
2019-02-28 22:28:26 -0500

    Ui elements

--- app/src/main/java/com/example/pc13/pingrequest/MainActivity.java
+++ app/src/main/java/com/example/pc13/pingrequest/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.pc13.pingrequest;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView currentConectivity;
+    TextView pingStatus;
+    Button pingICMP;
+    Button pingHTTP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentConectivity = (TextView) findViewById(R.id.current_network);
+        pingStatus = (TextView) findViewById(R.id.ping_status);
+
+        pingHTTP = (Button) findViewById(R.id.ping_http);
+        pingICMP = (Button) findViewById(R.id.ping_icmp);
+
+
+
+    }
+
+
+
+    void  curenrNetworkStatus(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (networkInfo.getTypeName().equalsIgnoreCase("WIFI")){
+            stringBuilder.append("You are connected through WIFI");
+        }else if(networkInfo.getTypeName().equalsIgnoreCase("Mobile")){
+            stringBuilder.append("You are connected through Mobile data");
+        }
+
+        currentConectivity.setText(stringBuilder);
+
     }
 }
---
+++ app/src/main/java/com/example/pc13/pingrequest/PingIp.java
@@ -0,0 +1,22 @@
+package com.example.pc13.pingrequest;
+
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+public class PingIp extends AsyncTask<URL, Integer, Long> {
+
+    public PingIp(String ip, int i){
+
+
+    }
+
+
+    @Override
+    protected Long doInBackground(URL... urls) {
+        return null;
+    }
+
+
+
+}
